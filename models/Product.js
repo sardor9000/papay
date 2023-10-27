e7037cfedf8b97eebb8f1d4875d42e14ce109d3d
Sardor <usok201802@gmail.com>
2023-10-28 01:09:56 +0900

    feat: restaurant user getting restaurant related products

--- models/Product.js
+++ models/Product.js
@@ -8,6 +8,23 @@ class Product {
         this.productModel = ProductModel;
     }
 
+
+    async getAllProductsDataResto(member) {
+        try{
+            member._id = shapeIntoMongooseObjectId(member._id)
+            const result = await this.productModel.find({
+                restaurant_mb_id: member._id
+            });
+            assert.ok(result, Definer.general_err1);
+            return result;
+        }catch(err){
+            throw err
+        }
+    }
+
+
+
+
     async addNewProductData(data, member) {
         try{
             data.restaurant_mb_id = shapeIntoMongooseObjectId(member._id);
@@ -16,7 +33,6 @@ class Product {
             const result = await new_product.save();
 
             assert.ok(result, Definer.product_err1);
-
             return result;
         } catch(err){
             throw err;
