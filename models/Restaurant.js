91c5752b0884f4eaa1f55ca491b893e67f84a8c5
Sardor <usok201802@gmail.com>
2023-11-09 00:25:04 +0900

    fix: modify to render real restaurant data on all-restaurant ejs

---
+++ models/Restaurant.js
@@ -0,0 +1,29 @@
+const MemberModel = require("../schema/member.model")
+const Definer = require("../lib/mistake");
+const assert = require("assert");
+
+
+class Restaurant {
+    constructor() {
+        this.memberModel = MemberModel;
+    }
+
+    async getAllRestaurantsData() {
+        try {
+            const result = await this.memberModel
+                .find({
+                    mb_type: "RESTAURANT",
+                })
+                .exec();
+            
+            assert(result, Definer.general_eer1);
+            return result;      
+        } catch (err) {
+            throw err;
+        }
+    }
+}
+
+
+
+module.exports = Restaurant;
