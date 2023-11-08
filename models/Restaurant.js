27bbfb7be4727f3579764433fa410bd15c59a801
Sardor <usok201802@gmail.com>
2023-11-09 02:59:50 +0900

    feat: develop update restaurant member top data data by Admin

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,6 +1,7 @@
 const MemberModel = require("../schema/member.model")
 const Definer = require("../lib/mistake");
 const assert = require("assert");
+const { shapeIntoMongooseObjectId } = require("../lib/config");
 
 
 class Restaurant {
@@ -16,8 +17,26 @@ class Restaurant {
                 })
                 .exec();
             
-            assert(result, Definer.general_eer1);
+            assert.ok(result, Definer.general_eer1);
             return result;      
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async updateRestaurantByAdminData(update_data) {
+        try {
+            const id = shapeIntoMongooseObjectId(update_data?.id);
+            const result = await this.memberModel
+                .findByIdAndUpdate( { _id: id }, update_data, {
+                    runValidators: true,
+                    lean: true,
+                    returnDocument: "after"
+                }
+            ).exec()
+
+            assert.ok(result, Definer.general_err1);
+            return result;          
         } catch (err) {
             throw err;
         }
